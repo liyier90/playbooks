d9e3b5d876aee9114d1d710cdd738b60482a9668
liyier90 <56420072+liyier90@users.noreply.github.com>
2025-08-23 11:05:29 +0800

    install python with pyenv

--- install.sh
+++ install.sh
@@ -3,55 +3,132 @@
 set -euo pipefail
 [[ "${TRACE:-0}" == "1" ]] && set -x
 
-conda_base_prefix="${CONDA_BASE_PREFIX:-${HOME}/conda}"
-ansible_env="${ANSIBLE_ENV:-ansible}"
+pyenv_installer_url='https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer'
+pyenv_mpdecimal_url='https://www.bytereef.org/software/mpdecimal/releases/mpdecimal-4.0.1.tar.gz'
+pyenv_python_version='3.11'
 
-log_info() {
-    local message=$1
-    printf 'INFO - %s - %s\n' "$(date +%Y%m%d-%H%M%S)" "${message}"
+# conda_base_prefix="${CONDA_BASE_PREFIX:-${HOME}/conda}"
+# ansible_env="${ANSIBLE_ENV:-ansible}"
+
+log() {
+    local level="${1}"
+    local message="${2}"
+    printf '%s - %s - %s\n' "${level}" "$(date +%Y%m%d-%H%M%S)" "${message}"
 }
 
-###################
-# Install Miniforge
-###################
-log_info 'Started Miniforge install'
+log_error() {
+    log 'ERROR' "${1}"
+    exit 1
+}
+
+log_info() {
+    log 'INFO' "${1}"
+}
+
+###############
+# Install pyenv
+###############
+log_info 'BEGIN pyenv install'
+pushd .
+
+pyenv_installer=$(curl -fsSL "${pyenv_installer_url}")
+if [[ -z "${pyenv_installer}" ]]; then
+    log_error 'Empty script'
+fi
+bash <<<"${pyenv_installer}"
+
+export PYENV_ROOT="${HOME}/.pyenv"
+[[ -d "${PYENV_ROOT}/bin" ]] && export PATH="${PYENV_ROOT}/bin${PATH:+:${PATH}}"
+eval "$(pyenv init - bash)"
+eval "$(pyenv virtualenv-init -)"
+
+log_info "$(pyenv --version)"
+
+popd
+log_info 'END pyenv install'
+
+################
+# Install Python
+################
+log_info 'BEGIN Python install'
+
+log_info '  BEGIN Dependencies install'
+
+sudo apt-get update
+sudo apt-get install -yq \
+    build-essential \
+    ccache \
+    cmake \
+    gdb \
+    lcov \
+    libb2-dev \
+    libbz2-dev \
+    libffi-dev \
+    libgdbm-compat-dev \
+    libgdbm-dev \
+    liblzma-dev \
+    libncurses5-dev \
+    libreadline6-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libzstd-dev \
+    lzma \
+    lzma-dev \
+    pkg-config \
+    strace \
+    tk-dev \
+    uuid-dev \
+    xvfb \
+    zlib1g-dev
+
+log_info '    BEGIN mpdecimal install'
 pushd .
 
 mkdir /tmp/mybuild
 cd /tmp/mybuild
-miniforge_installer="Miniforge3-$(uname)-$(uname -m).sh"
-wget "https://github.com/conda-forge/miniforge/releases/latest/download/${miniforge_installer}"
-chmod +x "${miniforge_installer}"
-"./${miniforge_installer}" -b -p "${conda_base_prefix}"
-[[ -f "${conda_base_prefix}/.condarc" ]] && rm "${conda_base_prefix}/.condarc"
+
+wget "${pyenv_mpdecimal_url}"
+tar -xvf mpdecimal-*.tar.gz 
+cd mpdecimal-*/
+
+./configure --prefix='/usr/local'
+make -j
+sudo make install
+
 rm -rf /tmp/mybuild
 
 popd
-log_info 'Finished Miniforge install'
+log_info '    END mpdecimal install'
 
-##########################
-# Configure Python Mirrors
-##########################
-log_info 'Started configuring Python Mirrors'
-log_info "Set conda channel URL=${CONDA_CHANNEL_URL}"
-log_info "Set pip index URL=${PIP_INDEX_URL}"
+log_info '  END Dependencies install'
 
-envsubst < files/condarc.template >> ~/.condarc
+pyenv install "${pyenv_python_version}"
 
-[[ -d ~/.pip ]] || mkdir -p ~/.pip
-envsubst < files/pipconf.template >> ~/.pip/pip.conf
+log_info 'END Python install'
 
-log_info 'Finished configuring Python Mirrors'
-
-#################
-# Install Ansible
-#################
-log_info 'Started Ansible install'
-
-# shellcheck source=/dev/null
-. "${conda_base_prefix}/etc/profile.d/conda.sh"
-conda env create -n "${ansible_env}" -f ansible.yml
-conda activate "${ansible_env}"
-ansible-galaxy install -f -r requirements.yml
-
-log_info 'Finished Ansible install'
+# ##########################
+# # Configure Python Mirrors
+# ##########################
+# log_info 'Started configuring Python Mirrors'
+# log_info "Set conda channel URL=${CONDA_CHANNEL_URL}"
+# log_info "Set pip index URL=${PIP_INDEX_URL}"
+# 
+# envsubst < files/condarc.template >> ~/.condarc
+# 
+# [[ -d ~/.pip ]] || mkdir -p ~/.pip
+# envsubst < files/pipconf.template >> ~/.pip/pip.conf
+# 
+# log_info 'Finished configuring Python Mirrors'
+# 
+# #################
+# # Install Ansible
+# #################
+# log_info 'Started Ansible install'
+# 
+# # shellcheck source=/dev/null
+# . "${conda_base_prefix}/etc/profile.d/conda.sh"
+# conda env create -n "${ansible_env}" -f ansible.yml
+# conda activate "${ansible_env}"
+# ansible-galaxy install -f -r requirements.yml
+# 
+# log_info 'Finished Ansible install'
